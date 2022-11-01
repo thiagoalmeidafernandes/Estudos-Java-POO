8ebf1cb200f3b2f149d5d7c3a48b70051b0ac5b3
Thiago de Almeida Correia Fernandes <110740750+thiagoalmeidafernandes@users.noreply.github.com>
2022-11-01 03:19:08 -0300

    Inicia Aula07 - Ultra Emoticon Fight

---
+++ Pasta Principal/Aula07b/src/Lutador.java
@@ -0,0 +1,150 @@
+public class Lutador {
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+
+    public Lutador(String nome,
+                   String nacionalidade,
+                   int idade,
+                   float altura,
+                   float peso,
+                   int vitorias,
+                   int derrotas,
+                   int empates) {
+
+        setNome(nome);
+        setNacionalidade(nacionalidade);
+        setIdade(idade);
+        setAltura(altura);
+        setPeso(peso);
+        setVitorias(vitorias);
+        setDerrotas(derrotas);
+        setEmpates(empates);
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria() {
+        if (this.getPeso() < 52.2f) {
+            categoria = "Inválido";
+        } else if (this.getPeso() <= 70.3f) {
+            categoria = "Leve";
+        } else if (this.getPeso() <= 83.9f) {
+            categoria = "Médio";
+        } else if (this.getPeso() <= 120.2f) {
+            categoria = "Pesado";
+        } else {
+            categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+
+    public void apresentar() {
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Origem: " + this.getNacionalidade());
+        System.out.println(this.getIdade() + " anos");
+        System.out.println(this.getAltura() + " m de altura");
+        System.out.println("Pesando " + this.getPeso() + "Kg");
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Perdeu: " + this.getDerrotas());
+        System.out.println("Empatou: " + this.getEmpates());
+    }
+
+    public void status() {
+        System.out.println(getNome());
+        System.out.println("é um peso " + this.getCategoria());
+        System.out.println(this.getVitorias() + " vitórias");
+        System.out.println(this.getDerrotas() + " derrotas");
+        System.out.println(this.getEmpates() + " empates");
+    }
+
+}
+
