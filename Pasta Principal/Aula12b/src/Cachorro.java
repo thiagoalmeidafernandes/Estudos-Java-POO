b1dca0830a42f1d26dee56620f2d045a1964d26a
Thiago de Almeida Correia Fernandes <110740750+thiagoalmeidafernandes@users.noreply.github.com>
2022-11-03 00:08:23 -0300

    Polimorfismo Sobrecarga

--- Pasta Principal/Aula12b/src/Cachorro.java
+++ Pasta Principal/Aula12b/src/Cachorro.java
@@ -3,4 +3,28 @@ public class Cachorro extends Mamifero{
     public void emitirSom() {
         System.out.println("Au! Au!");;
     }
+
+    public void reagir(String frase) {
+        if(frase.equals("Olá") || frase.equals("Toma comida")){
+            System.out.println("Abanar e latir");
+        } else {
+            System.out.println("Rosnar");
+        }
+    }
+
+    public void reagir(int hora, int min) {
+
+    }
+
+    public void reagir(boolean dono) {
+        if (dono) {
+            System.out.println("Feliz");
+        } else {
+            System.out.println("Triste");
+        }
+    }
+
+    public void reagir(int idade, float peso){
+
+    }
 }
